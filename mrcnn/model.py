c2307c9018d16654e687858e3b0fdd82badac1f5
Dima <wannafIy@protonmail.com>
2019-10-17 02:06:16 +0300

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2160,9 +2160,7 @@ class MaskRCNN():
         metrics. Then calls the Keras compile() function.
         """
         # Optimizer object
-        optimizer = keras.optimizers.SGD(
-            lr=learning_rate, momentum=momentum,
-            clipnorm=self.config.GRADIENT_CLIP_NORM)
+        optimizer = keras.optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, clipnorm=self.config.GRADIENT_CLIP_NORM, amsgrad=False)
         # Add Losses
         # First, clear previously set losses to avoid duplication
         self.keras_model._losses = []
